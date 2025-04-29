b4ba932b6f9f3ed99598e9f931058f22bbc1e636
Wendell Régis <r.wendell.regis@gmail.com>
2025-04-29 08:22:59 -0300

    Commit testes
    Commit teste

---
+++ src/components/card-funcionalidade/card-funcionalidade.tsx
@@ -0,0 +1,12 @@
+function CardFuncionalidade() {
+  return (
+    <div className="card">
+      <img src="https://via.placeholder.com/150" alt="Camisa" />
+      <h2>Camisa</h2>
+      <p>Descrição da camisa.</p>
+      <button>Comprar</button>
+    </div>
+  );
+}
+
+export default CardFuncionalidade;
