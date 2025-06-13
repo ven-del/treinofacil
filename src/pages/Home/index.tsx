afef303a4dc753c6b958e13142edc086d04b1e06
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-13 10:01:15 -0300

    Criado o primeiro card
    Criado o primeiro card. Commit feito para ter um backup antes de instalar o postcss e o autoprefixer.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import StudentSection from "../../sections/StudentSection";
+
+const Home = () => {
+    return (  
+        <main>
+            <section>
+                <StudentSection />
+            </section>
+        </main>
+    );
+}
+ 
+export default Home;
---
+++ src/sections/StudentSection/index.tsx
@@ -0,0 +1,23 @@
+const StudentSection = () => {
+  return (
+    <div>
+      <h1>Para Alunos</h1>
+      <div>
+        <div className="card mt-40 w-fit">
+          <img
+            src="/assets/images/card_aluno_1.png"
+            alt="Imagem gerada por IA de alguém fazendo um agachamento, mostrando passo a passo"
+          />
+          <h3>Visualização de exercícios</h3>
+          <p>
+            Edite pesos, repetições, adicione notas e visualize guias visuais
+            para cada exercício.
+          </p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default StudentSection;
